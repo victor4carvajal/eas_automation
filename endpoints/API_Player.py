dd1c1ab69a27d292184fb814d913bf0c21bca1bd
Victor3Carvajal <victor@methoddata.com>
2024-07-23 20:29:58 -0500

    setting up for second api

--- endpoints/API_Player.py
+++ endpoints/API_Player.py
@@ -8,23 +8,24 @@ from .API_Interface import API_Interface
 from .API_Player_Auth_Engine import API_Player_Auth_Engine
 from utils.results import Results
 import logging
-
+import conf.utils_conf.base_url_conf as base_url_conf
 
 class API_Player(Results):
     "The class that maintains the test context/state"
 
-    def __init__(self, url, log_file_path=None, session_flag=False):
+    def __init__(self, auth_url, aes_url, log_file_path=None, session_flag=False):
         "Constructor"
         super(API_Player, self).__init__(
             level=logging.DEBUG, log_file_path=log_file_path)
-        self.api_obj = API_Interface(url=url, session_flag=session_flag)
+        self.api_obj = API_Interface(auth_url=base_url_conf.auth_api_base_url, aes_url=base_url_conf.aes_api_base_url, session_flag=session_flag)
         self.log_file_path = log_file_path
 
-    def get_api_engine_object(self,engine_name):
+    def get_api_engine_object(self, engine_name):
         "Return the appropriate api object based on engine name"
         api_engine_obj = None
         api_engine_name = engine_name.lower()
-        if api_engine_name in ["auth","auth engine"]:
-            api_engine_obj = API_Player_Auth_Engine(self.api_obj,log_file_path=self.log_file_path) 
+        if api_engine_name in ["auth", "auth engine"]:
+            api_engine_obj = API_Player_Auth_Engine(self.api_obj, log_file_path=self.log_file_path)
         
-        return api_engine_obj      
+        return api_engine_obj
+    
