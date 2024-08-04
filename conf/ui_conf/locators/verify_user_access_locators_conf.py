2fdc464fb3be271b89d97725b1c6e8c5a34056ee
Victor3Carvajal <victor@methoddata.com>
2024-08-04 17:42:41 -0500

    Adding resend code ui

--- conf/ui_conf/locators/verify_user_access_locators_conf.py
+++ conf/ui_conf/locators/verify_user_access_locators_conf.py
@@ -1,3 +1,4 @@
 code_input = "xpath,//ng-otp-input//input"
 submit_button = "xpath,//auth-verify-user-access//div[2]//button[2]"
 verify_user_access_panel = "xpath,(//auth-verify-user-access//div)[5]"
+resend_code_button = "xpath,//p/span[contains(text(), 'Resend code')]"
--- page_objects/verify_user_access_page.py
+++ page_objects/verify_user_access_page.py
@@ -31,24 +31,34 @@ class Verify_User_Access_Page(Base_Page):
                                negative='could not click submit button')
         return result_flag
 
-    def verify_dashboard_panel(self):
-        "check the dashboard panel"
-        result_flag = self.smart_wait(dashboard_locators.dashboard_panel,wait_seconds=15)
+    def verify_user_name(self):
+        "check the user name"
+        result_flag = self.smart_wait(dashboard_locators.user_button,wait_seconds=15)
         self.conditional_write(result_flag,
-                               positive='Dashboard panel present',
-                               negative='could not find dashboard panel')
+                               positive='User name is present',
+                               negative='could not find user name')
         return result_flag
 
     @Wrapit._screenshot
     def enter_code(self,code):
         "enter validation code"
+        self.start()
         self.set_code(code)
         result_flag = self.click_submit()
-        result_flag &= self.verify_dashboard_panel()
+        result_flag &= self.verify_user_name()
         self.conditional_write(result_flag,
                                positive='logged into the app',
                                negative='could not login')
         if result_flag:
             self.switch_page("dashboard")
         return result_flag
+    
+    def resend_code(self):
+        "Resend validation code"
+        self.start()
+        self.click_element(locators.resend_code_button)
+        result_flag = self.click_element(locators.resend_code_button)
+        self.conditional_write(result_flag,
+                               positive='clicked the resend code button',
+                               negative='could not click resend code button')
     
